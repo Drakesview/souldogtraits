fd0bdbe991e4572f9b563e3de77c38ba59e19d27
hachiko-souldogs <hachikosouldogs@gmail.com>
2022-04-16 14:13:59 -0700

    adjusted header style

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ function Header() {
       <div className="relative flex items-center align-middle h-24 cursor-pointer">
         <Image src="/images/PAW-03.png" layout="fill" objectFit="contain" />
       </div>
-      <div className="flex items-center justify-end space-x-4 sm:col-span-4">
+      <div className="flex items-center justify-end space-x-2 sm:col-span-4">
         <Link href="https://twitter.com/SoulDogsNFT">
           <a target="_blank" className="none">
             <FaTwitter className="h-8 cursor-pointer w-8 fill-white hover:scale-105 transform transition duration-300 ease-out" />
@@ -23,14 +23,14 @@ function Header() {
         </Link>
         <Link href={"http://Discord.gg/SoulDogs"}>
           <a target="_blank" className="none">
-            <FaDiscord className="h-8 cursor-pointer w-8 fill-white hover:scale-105 transform transition duration-300 ease-out" />
+            <FaDiscord className="h-8 cursor-pointer w-8 ml-2 fill-white hover:scale-105 transform transition duration-300 ease-out" />
           </a>
         </Link>
         <a
           href="http://www.souldogs.city"
           target="_blank"
           rel="noopener noreferrer"
-          className="lg:block text-white hover:scale-105 transform transition duration-300 ease-out  px-4 py-2 rounded-lg
+          className="lg:block text-white hover:scale-105 transform transition duration-300 ease-out px-2 py-2 rounded-lg
           font-luckiestGuy sm:text-2xl"
         >
           Home
@@ -39,7 +39,7 @@ function Header() {
           href="http://shop.souldogs.city"
           target="_blank"
           rel="noopener noreferrer"
-          className="lg:block text-white hover:scale-105 transform transition duration-300 ease-out  px-4 py-2 rounded-lg
+          className="lg:block text-white hover:scale-105 transform transition duration-300 ease-out px-2 py-2 rounded-lg
           font-luckiestGuy sm:text-2xl text-lg"
         >
           Shop
